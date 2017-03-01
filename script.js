b96e4a93dd2e7ce6f63c213f484e89000c0471bf
Kiara Correa Acosta <kcorrea@aclanyc.com>
2017-03-01 13:34:34 +0000

    I have changed stuff

---
+++ script.js
@@ -0,0 +1,17 @@
+  TinyTurtle.apply(window); 
+  
+  forward(30);
+  left(90);
+  forward(20);
+  right(135);
+  forward(40);
+  
+  right(90);
+  forward(40);
+  right(135);
+  forward(20);
+  
+  left(90);
+  forward(30);
+  right(90);
+  forward(16);
